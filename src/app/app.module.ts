aa3dafd5fe337141d1de956bb898b84d4d0d427b
Gabriel <gabrielcorbot@gmail.com>
2023-02-07 15:56:29 -0500

    Componentes del Home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { InfoComponent } from './components/info/info.component';
+import { HelpComponent } from './components/help/help.component';
+import { MoodCardComponent } from './components/home/mood-card/mood-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InfoComponent,
+    HelpComponent,
+    MoodCardComponent
   ],
   imports: [
     BrowserModule,
