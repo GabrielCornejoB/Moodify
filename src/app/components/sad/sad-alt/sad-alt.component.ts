b454ce4289f7e5eaa43d927e366cc110a74b4aa1
MuraJJDG <juanj.duque@upb.edu.co>
2023-02-22 21:19:12 -0500

    Carpeta gifs creados, y componente sad-alt estructura terminada

--- src/app/components/sad/sad-alt/sad-alt.component.ts
+++ src/app/components/sad/sad-alt/sad-alt.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./sad-alt.component.sass']
 })
 export class SadAltComponent {
+  sadgifs:string = "../../../../assets/gifs/sad-gifs/";
+  array1:string[] = ["1.gif", "2.gif","3.gif", "4.gif","5.gif", "6.gif","7.gif", "8.gif","9.gif"];
+  concatenatedArray:string[] = [];
 
+  constructor() {
+    this.concatenatedArray = this.array1.map((element) => {
+      return this.sadgifs + element;
+    });
+    console.log(this.concatenatedArray);
+  }
 }
